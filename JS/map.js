4b5f0bff2edf67a85f027636aba65fc259603b85
Alexandr Cheremisin <an-cheremisin@yandex.ru>
2017-08-22 22:35:39 +0300

    Делаю объект v_03

--- JS/map.js
+++ JS/map.js
@@ -13,60 +13,62 @@ var IMAGE_ADRESS_PHOTOS ='';
 var TYPES_OF_ACCOMMODATION =['flat', 'house', 'bungalo'];
 var TIME_OF_ARRIVAL = ['12:00', '13:00', '14:00'];
 var ADVANTAGES =['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 var locationNearbyX = getRandomInt(300, 900);
 var locationNearbyY = getRandomInt(100, 500);
 
-var getRandomInt = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 var avatarSelection = function(number) {
   if (number <=8){
     IMAGE_ADRESS_PHOTOS ='img/avatars/user 0'+ number +'.png';
     return IMAGE_ADRESS_PHOTOS;
   } else {return "Нет аватара."};
-} 
+}; 
 
 var shortDescription = function(numberTitle, namesDirectory){
   if (numberTitle <=namesDirectory.length){
     return namesDirectory[numberTitle];
   }
-}
+};
 
-var getRandomArrayElements = function(numberArray,arr){
+var getRandomArrayElements = function(arr){
+  var arr1=[];
   for (i=0; i <= arr.length; i++){
-    if (Math.random(numberArray)>0.5){
-      return arr[i];
+    if (Math.random()>0.5){
+      arr1[i]= i;
     } 
   }
-}
+  return arr1.length;
+};
 
-var createNewAds = function(number, numberTitle, numberArray){
+var createNewAds = function(number, numberTitle){
   var newAds = {
-    author = {
+    author:{
       avatar: avatarSelection(number)
     },
-    offer = {
+    offer:{
       title: shortDescription(numberTitle,TYPES_OF_HOUSING),
-      address: ['locationNearbyX', 'locationNearbyX'],
+      address: [locationNearbyX, locationNearbyX],
       price: getRandomInt(1000, 1000000),
       type: shortDescription(numberTitle, TYPES_OF_ACCOMMODATION),
       rooms: getRandomInt(1, 5),
       guests: getRandomInt(1, 5),
       checkin: shortDescription(numberTitle,TIME_OF_ARRIVAL),
       checkout: shortDescription(numberTitle,TIME_OF_ARRIVAL),
-      features: getRandomArrayElements(numberArray, ADVANTAGES),
+      features: ADVANTAGES[getRandomArrayElements(ADVANTAGES)],
       description: '',
       photos: []
     },
-    location = {
+    location:{
       x: locationNearbyX,
       y: locationNearbyY
     }
   }
   return newAds;
-}
-
+};
+createNewAds(getRandomInt(1, 8),getRandomInt(1, 8));
 /*var similarAds = function() {
   for (i=0; 
 };*/
